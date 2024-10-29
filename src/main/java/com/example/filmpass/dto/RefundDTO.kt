ed0b77306ba138d72370e2e052604b28aadda2ba
LChaeWon <dcodnjs12@naver.com>
2024-10-29 17:31:11 +0900

    feat : PaymentService 변환

--- src/main/java/com/example/filmpass/dto/RefundDTO.kt
+++ src/main/java/com/example/filmpass/dto/RefundDTO.kt
@@ -5,11 +5,8 @@ import lombok.Data
 import lombok.NoArgsConstructor
 import java.time.LocalDateTime
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 class RefundDTO {
-    private val reserveId: Long? = null // 주문 번호
-    private val amount: Int? = null // 환불할 금액
-    private val refundRequestDate: LocalDateTime? = null // 환불 요청 날짜
+    val reserveId: Long? = null // 주문 번호
+    val amount: Int? = null // 환불할 금액
+    var refundRequestDate: LocalDateTime? = null // 환불 요청 날짜
 }
--- src/main/java/com/example/filmpass/entity/Payment.kt
+++ src/main/java/com/example/filmpass/entity/Payment.kt
@@ -21,37 +21,37 @@ import java.time.LocalDateTime
 class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private val PaymentId: Long? = null
+    val PaymentId: Long? = null
 
     @Column(name = "paymentStatus")
     @Enumerated(EnumType.STRING)
-    private var status: PayStatus? = null
+    var status: PayStatus? = null
 
-    private val amount: Int? = null
+    var amount: Int? = null
 
-    private val payToken: String? = null //토스 결제하면 생성되는 토큰
+    var payToken: String? = null //토스 결제하면 생성되는 토큰
 
     @Column(name = "paymentType")
     @Enumerated(EnumType.STRING)
-    private var payType: PayType? = null
+    var payType: PayType? = null
 
-    private val paidTs: LocalDateTime? = null //결제 일시
+    var paidTs: LocalDateTime? = null //결제 일시
 
-    private val refundAmount = 0 // 환불된 금액 (부분 환불을 위해)
+    var refundAmount = 0 // 환불된 금액 (부분 환불을 위해)
 
-    private val availableRefundAmount: Int? = null // 결제금액 - 누적된 횐불 금액 값이 들어감
+    var availableRefundAmount: Int? = null // 결제금액 - 누적된 횐불 금액 값이 들어감
 
     @Enumerated(EnumType.STRING) // Enum을 String으로 저장
-    private val refundStatus: RefundStatus? = null
+    var refundStatus: RefundStatus? = null
 
-    private val refundRequestDate: LocalDateTime? = null // 환불 요청 날짜
+    var refundRequestDate: LocalDateTime? = null // 환불 요청 날짜
 
-    private val refundDate: LocalDateTime? = null // 환불 처리 날짜
+    var refundDate: LocalDateTime? = null // 환불 처리 날짜
 
 
-    private val createdTs: LocalDateTime? = null
+    var createdTs: LocalDateTime? = null
 
     @OneToOne //예매랑 연결되면 추가하기
     @JoinColumn(name = "reserve_id")
-    private val reservation: Reservation? = null
+    var reservation: Reservation? = null
 }
